f85344220e5a13b66789ad13b470181f42ebccf3
Matthias Drescher <matthias_p_drescher@t-online.com>
2016-08-29 17:22:00 +0200

    using functions to clean up printing mess

--- src/main.rs
+++ src/main.rs
@@ -95,23 +95,11 @@ fn print(mut term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, colo
 {
 	let mut lines_printed = 13;
 
-
-	//======================================
 	//CPU
-	//======================================
 
-
-	let _ = write!(term, "========================== ");
-	colorize(&mut term, color, color::YELLOW);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, "CPU");
-	reset(&mut term, color);
-	let _ = writeln!(term, " ==========================");
-
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, "{}", cpu.processes);
-	reset(&mut term, color);
+	//"x processes on x cores"
+	print_header(term, color, 57, String::from("CPU"));
+	print_highlighted(term, color, format!("{}", cpu.processes));
 	if cpu.processes > 1
 	{
 		let _ = write!(term, " processes on ");	
@@ -119,109 +107,61 @@ fn print(mut term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, colo
 	{
 	   	let _ = write!(term, " process on ");
 	}
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, "{}", cpu.cores);
-	reset(&mut term, color);
+	print_highlighted(term, color, format!("{}", cpu.cores));
 	let _ = writeln!(term, " cores      ");
 	let _ = writeln!(term, "");
 
-	print_cpu_load(&mut term, color, &cpu.total_load, String::from("TOTAL"), true);
+	//print bars
+	print_highlighted(term, color, String::from("TOTAL: "));
+	let total_percentage = calc_cpu_load_percentage(&cpu.total_load);
+	print_progress_bar(term, color, total_percentage, 40, color::RED);
+	print_highlighted(term, color, format!(" {} %   ", format_float(total_percentage)));
+	let _ = writeln!(term, "");
+	
 	let mut core_counter = 1;
 	for core_load in &cpu.cores_load
 	{
-		print_cpu_load(term, color, core_load, format!("CPU {}", core_counter), false);
-		core_counter += 1;
+		let _ = write!(term, "CPU {}: ", core_counter);
+		let core_percentage = calc_cpu_load_percentage(&core_load);
+		print_progress_bar(term, color, core_percentage, 40, color::GREEN);
+		let _ = write!(term, " {} %   ", format_float(core_percentage));
+		let _ = writeln!(term, "");
 		lines_printed += 1;
+		core_counter += 1;
 	}
 	let _ = writeln!(term, "");
 
-
-	//======================================
 	//MEMORY
-	//======================================
 
-
-	let _ = write!(term, "========================= ");
-	colorize(&mut term, color, color::YELLOW);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, "MEMORY");
-	reset(&mut term, color);
-	let _ = writeln!(term, " ========================");
+	print_header(term, color, 57, String::from("MEMORY"));
 	let _ = writeln!(term, "");
 
 	let memory_use: f64 = (mem.total - mem.free - mem.cached) as f64 / mem.total as f64;
-	let memory_bar_size = (memory_use * 40.0) as usize;
 	let swap_use: f64 = (mem.swap_total - mem.swap_free) as f64 / mem.swap_total as f64;
-	let swap_bar_size = (swap_use * 40.0) as usize;
 
-	let _ = write!(term, "  RAM: ["); //RAM BAR
-	attribute(&mut term, color, Attr::Bold);
-	colorize(&mut term, color, color::GREEN);
-	for i in 0..40
-	{
-		if i < memory_bar_size
-		{
-			let _ = write!(term, "=");
-		}
-		else 
-		{
-		    let _ = write!(term, " ");
-		}
-	}
-	reset(&mut term, color);
-	let _ = writeln!(term, "]");
-
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ =  write!(term, "             {}", format_gib(mem.total - mem.free - mem.cached));
-	reset(&mut term, color);
+	let _ = write!(term, "  RAM: "); //RAM BAR
+	print_progress_bar(term, color, memory_use, 40, color::GREEN);
+	let _ = writeln!(term, "");
+	print_highlighted(term, color, format!("             {}", format_gib(mem.total - mem.free - mem.cached)));
 	let _ = write!(term, " GiB / ");
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ =  write!(term, "{}", format_gib(mem.total));
-	reset(&mut term, color);
+	print_highlighted(term, color, format!("{}", format_gib(mem.total)));
 	let _ = write!(term, " GiB (");
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, " {}% ", format_float(memory_use));
-	reset(&mut term, color);
+	print_highlighted(term, color, format!(" {}% ", format_float(memory_use)));
 	let _ = write!(term, ")");
 	let _ = writeln!(term, "\n");
 
-	let _ = write!(term, " SWAP: ["); //SWAP BAR
-	attribute(&mut term, color, Attr::Bold);
-	colorize(&mut term, color, color::GREEN);
-	for i in 0..40
-	{
-		if i < swap_bar_size
-		{
-			let _ = write!(term, "=");
-		}
-		else 
-		{
-		    let _ = write!(term, " ");
-		}
-	}
-	reset(&mut term, color);
-	let _ = writeln!(term, "]");
-
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ =  write!(term, "               {}", format_gib(mem.swap_used));
-	reset(&mut term, color);
+	let _ = write!(term, " SWAP: "); //SWAP BAR
+	print_progress_bar(term, color, swap_use, 40, color::GREEN);
+	let _ = writeln!(term, "");
+	print_highlighted(term, color, format!("               {}", format_gib(mem.swap_used)));
 	let _ = write!(term, " GiB / ");
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ =  write!(term, "{}", format_gib(mem.swap_total));
-	reset(&mut term, color);
+	print_highlighted(term, color, format!("{}", format_gib(mem.swap_total)));
 	let _ = write!(term, " GiB (");
-	colorize(&mut term, color, color::CYAN);
-	attribute(&mut term, color, Attr::Bold);
-	let _ = write!(term, " {}% ", format_float(swap_use));
-	reset(&mut term, color);
+	print_highlighted(term, color, format!(" {}% ", format_float(swap_use)));
 	let _ = write!(term, ")");
 	let _ = writeln!(term, "\n");
+
+
 
 	for _ in 0..lines_printed
 	{
@@ -229,38 +169,15 @@ fn print(mut term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, colo
 	}
 }
 
-fn print_cpu_load(mut term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, color: bool, load: &CPULoad, name: String, total: bool)
+//UI Objects
+
+fn print_progress_bar(term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, enabled: bool, value: f64, size: usize, color: u16)
 {
-	if total
-	{
-		colorize(&mut term, color, color::CYAN);
-		attribute(&mut term, color, Attr::Bold);
-	}
-	let _ = write!(term, "{}:", name);
-	if total
-	{
-		reset(&mut term, color);
-	}
-	let _ = write!(term, " [");
-	
-	let mut load_percentage: f64 = 0.0;
-	if load.busy != 0
-	{
-		load_percentage = load.busy as f64 / (load.idle + load.busy) as f64;
-	}
-	let barsize = (load_percentage * 40.0) as usize;
-
-	if !total
-	{
-		colorize(&mut term, color, color::GREEN);
-	}
-	else 
-	{
-	 	colorize(&mut term, color, color::RED);
-	}
-	attribute(&mut term, color, Attr::Bold);
-
-	for i in 0..40
+	let barsize = ((value * size as f64)) as usize;
+	let _ = write!(term, "[");
+	colorize(term, enabled, color);
+	attribute(term, enabled, Attr::Bold);
+	for i in 0..size
 	{
 		if i < barsize
 		{
@@ -268,21 +185,45 @@ fn print_cpu_load(mut term: &mut Box<term::Terminal<Output=std::io::Stdout> + Se
 		}
 		else 
 		{
-		    let _ = write!(term, " ");
+		 	let _ = write!(term, " ");   
 		}
 	}
-
-	reset(&mut term, color);
-	let _ = write!(term, "] ");
-	reset(&mut term, color);
-	if total
-	{
-		attribute(&mut term, color, Attr::Bold);
-		colorize(&mut term, color, color::CYAN);
-	}
-	let _ = writeln!(term, "{} %   ", format_float(load_percentage));
-	reset(&mut term, color);
+	reset(term, enabled);
+	let _ = write!(term, "]");
 }
+
+fn print_highlighted(term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, enabled: bool, content: String)
+{
+	colorize(term, enabled, color::CYAN);
+	attribute(term, enabled, Attr::Bold);
+	write!(term, "{}", content);
+	reset(term, enabled);
+}
+
+fn print_header(term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, enabled: bool, size: usize, name: String)
+{
+	let halfsize = (size - name.len() - 2) / 2;
+	let extend_first = halfsize * 2 + name.len() + 2 != size; //catch rounding errors
+	for _ in 0..halfsize
+	{
+		let _ = write!(term, "=");
+	}
+	if extend_first
+	{
+		let _ = write!(term, "=");
+	}
+	colorize(term, enabled, color::YELLOW);
+	attribute(term, enabled, Attr::Bold);
+	let _ = write!(term, " {} ", name);
+	reset(term, enabled);
+	for _ in 0..halfsize
+	{
+		let _ = write!(term, "=");
+	}
+	let _ = writeln!(term, "");
+}	
+
+//HELPER FUNCTIONS
 
 fn attribute(term: &mut Box<term::Terminal<Output=std::io::Stdout> + Send>, enabled: bool, attrib: Attr)
 {
@@ -329,4 +270,14 @@ fn format_gib(kib: u64) -> String
 		string = string.split_at(string.find(".").unwrap() + 2).0.to_owned();
 	}
 	string
+}
+
+fn calc_cpu_load_percentage(load: &CPULoad) -> f64
+{
+	let mut load_percentage: f64 = 0.0;
+	if load.busy != 0
+	{
+		load_percentage = load.busy as f64 / (load.idle + load.busy) as f64;
+	}
+	load_percentage
 }
